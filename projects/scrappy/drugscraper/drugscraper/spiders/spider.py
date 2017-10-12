c04325c0bbd5876dae01c599a5959e4a46b6264b
Ben Liyanage <BenLiyanage@users.noreply.github.com>
2017-10-11 17:44:37 -0700

    working spider

--- projects/scrappy/drugscraper/drugscraper/spiders/spider.py
+++ projects/scrappy/drugscraper/drugscraper/spiders/spider.py
@@ -1,13 +1,25 @@
+import json
 import os
 import scrapy
+from bs4 import BeautifulSoup
 
 
 class Drugs(scrapy.Spider):
     name = "DrugScraper"
-    starts_urls = [
-        # 'https://en.wikipedia.org/wiki/Category:Drugs_by_target_organ_system'
-        'https://en.wikipedia.org/wiki/Omeprazole'
-    ]
+    filemask = "./data/{}/{}"
+
+    # Constants for file position
+    HALF_LIFE = 0
+    HALF_LIFE_RAW = 3
+    DRUG_BANK = 1
+    DRUG_BANK_RAW = 4
+    DRUG_CLASS = 2
+    DRUG_CLASS_RAW = 5
+
+    custom_settings = {
+        'DEPTH_LIMIT': 2,
+    }
+
     def start_requests(self):
         try:
             os.remove('stats.csv')
@@ -15,8 +27,9 @@ class Drugs(scrapy.Spider):
             pass
 
         urls = [
-            # 'https://en.wikipedia.org/wiki/Category:Drugs_by_target_organ_system'
-            'https://en.wikipedia.org/wiki/Omeprazole'
+            'https://en.wikipedia.org/wiki/Category:Drugs_by_target_organ_system'
+            # 'https://en.wikipedia.org/wiki/Omeprazole'
+            # 'https://en.wikipedia.org/wiki/Vemurafenib'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -27,57 +40,66 @@ class Drugs(scrapy.Spider):
             return self.parse_category(response)
         else:
             self.parse_drug(response)
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-
-
-        # next_page = response.css('li.next a::attr(href)').extract_first()
-        # if next_page is not None:
-        # next_page = response.urljoin(next_page)
-        # yield scrapy.Request(next_page, callback=self.parse)
-
-        # for a in response.css('li.next a'):
-        #     yield response.follow(a, callback=self.parse)
-
-        import pdb
-
-        pdb.set_trace()
 
     def parse_category(self, response):
-        self.log(response.css('.mw-category a').extract())
+        # self.log(response.css('.mw-category a').extract())
         for a in response.css('.mw-category a'):
             yield response.follow(a, callback=self.parse)
 
     def parse_drug(self, response):
+        drug_name = response.url.split('/')[-1]
 
-        stats = [None] * 3
-        HALF_LIFE = 0
-        HALF_LIFE_RAW = 3
-        DRUG_BANK = 1
-        DRUG_BANK_RAW = 4
-        DRUG_CLASS = 2
-        DRUG_CLASS_RAW = 5
+        self.save_string(drug_name, response.text, 'page.html')
+        self.save_json(response, drug_name)
 
+    def save_json(self, response, drug_name):
         # Loop over the rows of the info box
+
+        stats = [''] * 6
+        infobox = {}
+        data = {
+            'url': response.url,
+            'infobox': infobox
+        }
+
+        section_header = None
         for item in (response.css('.infobox tr')):
-            link_title = item.css('th a::attr(title)').extract_first()
-            value = item.css('td')
-            print(link_title)
-            print(item.css('th').extract_first())
+            header_fingerprint = item.css('th::attr(style)').extract_first()
+            if header_fingerprint == 'text-align:center;background:#ddd':
+                # returns an array of text, so lets make it a single string.
+                section_header = BeautifulSoup(item.extract(), 'lxml').get_text().strip()
+            else:
+                raw_link_title = item.css('th').extract_first()
 
-            if link_title == 'Biological half-life':
-                stats[HALF_LIFE] = value.css('td::text').extract()
-                stats[HALF_LIFE_RAW] = value
-            elif link_title == 'DrugBank':
-                stats[DRUG_BANK] = value.css('a::text').extract()
-                stats[DRUG_BANK_RAW] = value
-            elif link_title == 'Drug class':
-                stats[DRUG_CLASS] = value.css('a::text')
-                stats[DRUG_CLASS_RAW] = value
-                # filename = 'by_system.html'
-                # with open(filename, 'wb') as f:
-                #     f.write(response.body)
-                # self.log('Saved file %s' % filename)
+                # This is skipping the chemical diagram
+                if not raw_link_title:
+                    continue
 
-        import pdb
-        pdb.set_trace()
+                link_title = BeautifulSoup(item.css('th').extract_first(), 'lxml').get_text()
+                raw_value = '$$$'.join(item.css('td').extract())
+                value = BeautifulSoup(raw_value, 'lxml').get_text().strip()
+                infobox[link_title] = {'value': value, 'raw': raw_value, 'section_header': section_header}
+
+                if link_title == 'Biological half-life':
+                    stats[self.HALF_LIFE] = value
+                    stats[self.HALF_LIFE_RAW] = raw_value
+                elif link_title == 'DrugBank':
+                    stats[self.DRUG_BANK] = value
+                    stats[self.DRUG_BANK_RAW] = raw_value
+                elif link_title == 'Drug class':
+                    stats[self.DRUG_CLASS] = value
+                    stats[self.DRUG_CLASS_RAW] = raw_value
+
+        # Using this as a fingerprint for this being a page about a drug...
+        # Probably not the best fingerprint, but it works ok given the small scope.
+        if len(infobox.keys()):
+            self.save_string(drug_name, json.dumps(data), 'data.json')
+            with open('./stats.csv', 'a') as stats_file:
+                stats_file.write('"{}", "{}"\n'.format('", "'.join(stats[0:3]), response.url))
+
+    def save_string(self, drug_name, string, filename):
+        drug_folder = './data/{}'.format(drug_name)
+        drug_filename = '{}/{}'.format(drug_folder, filename)
+        os.makedirs(drug_folder, exist_ok=True)
+        with open(drug_filename, 'w') as drug_file:
+            drug_file.write(string)
